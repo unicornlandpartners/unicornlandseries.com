819b8664ed0033475c16f8fea1437b144724808e
unicornlandpartners <unicornland.the.webseries@gmail.com>
2016-03-29 15:48:51 -0400

    scaffold custom sass styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,37 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    // copy the latest compiled version of bootstrap to our dist/ folder
     copy: {
-      // copy the latest compiled version of bootstrap to our dist/ folder
       prereqs: {
-        files: [
-          {
-            expand: true,
-            cwd: 'node_modules/bootstrap/dist/',
-            src: 'css/bootstrap.min.css',
-            dest: 'dist/'
-          },
-          {
-            expand: true,
-            cwd: 'node_modules/bootstrap/dist/',
-            src: 'js/bootstrap.min.js',
-            dest: 'dist/'
-          },
-        ],
+        files: [{
+          expand: true,
+          cwd: 'node_modules/bootstrap/dist/',
+          src: 'css/bootstrap.min.css',
+          dest: 'dist/'
+        },{
+          expand: true,
+          cwd: 'node_modules/bootstrap/dist/',
+          src: 'js/bootstrap.min.js',
+          dest: 'dist/'
+        }],
       },
     },
 
+    //
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none',
+          style: 'compressed'
+        },
+        files: {
+          'dist/css/screen.css': 'styles/screen.scss'
+        },
+      },
+    },
+
+    // commit contents of dist folder to gh-pages branch
     'gh-pages': {
       options: { base: 'dist' },
       src: ['**']
@@ -29,7 +40,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('default', ['copy', 'gh-pages']);
+  grunt.registerTask('default', ['copy', 'sass', 'gh-pages']);
 };
